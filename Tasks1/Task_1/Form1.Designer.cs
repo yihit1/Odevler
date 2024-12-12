dd3e4ee5957ff1c7b502f1048b99ebdf443f85f3
yihit1 <yiitc@outlook.com>
2024-12-12 21:52:03 +0300

    odev_1
    Hocam Labellar yerine Combobox lar ekledim bir de değerler random geliyo onun dışında oky

---
+++ Tasks1/Task_1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBoxEyeColor = new ComboBox();
+            comboBoxPlace = new ComboBox();
+            comboBoxHairColor = new ComboBox();
+            comboBoxNames = new ComboBox();
+            SuspendLayout();
+            // 
+            // comboBoxEyeColor
+            // 
+            comboBoxEyeColor.FormattingEnabled = true;
+            comboBoxEyeColor.Location = new Point(511, 79);
+            comboBoxEyeColor.Name = "comboBoxEyeColor";
+            comboBoxEyeColor.Size = new Size(121, 23);
+            comboBoxEyeColor.TabIndex = 0;
+            // 
+            // comboBoxPlace
+            // 
+            comboBoxPlace.FormattingEnabled = true;
+            comboBoxPlace.Location = new Point(372, 79);
+            comboBoxPlace.Name = "comboBoxPlace";
+            comboBoxPlace.Size = new Size(121, 23);
+            comboBoxPlace.TabIndex = 1;
+            // 
+            // comboBoxHairColor
+            // 
+            comboBoxHairColor.FormattingEnabled = true;
+            comboBoxHairColor.Location = new Point(233, 79);
+            comboBoxHairColor.Name = "comboBoxHairColor";
+            comboBoxHairColor.Size = new Size(121, 23);
+            comboBoxHairColor.TabIndex = 2;
+            comboBoxHairColor.SelectedIndexChanged += eyeColors_SelectedIndexChanged;
+            // 
+            // comboBoxNames
+            // 
+            comboBoxNames.FormattingEnabled = true;
+            comboBoxNames.Location = new Point(233, 16);
+            comboBoxNames.Name = "comboBoxNames";
+            comboBoxNames.Size = new Size(121, 23);
+            comboBoxNames.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBoxNames);
+            Controls.Add(comboBoxHairColor);
+            Controls.Add(comboBoxPlace);
+            Controls.Add(comboBoxEyeColor);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBoxEyeColor;
+        private ComboBox comboBoxPlace;
+        private ComboBox comboBoxHairColor;
+        private ComboBox comboBoxNames;
+    }
+}
---
+++ Tasks1/Task_1/Form1.cs
@@ -0,0 +1,62 @@
+﻿namespace Task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComboboxes();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void InitializeComboboxes()
+        {
+            // Names to populate the first combobox
+            List<string> names = new List<string> { "Gökberk", "Mahmut", "Şimal", "Emine", "Eren", "Yiğit", "Veli" };
+            comboBoxNames.DataSource = names;
+
+            // Hair color list (all are black)
+            List<string> hairColors = new List<string> { "Black" };
+            comboBoxHairColor.DataSource = hairColors;
+
+            // Eye color list
+            List<string> eyeColors = new List<string> { "Green", "Brown", "Black", "Blue", "Purple" };
+            comboBoxEyeColor.DataSource = eyeColors;
+
+            // Place of living list
+            List<string> places = new List<string> { "Çanakkale", "İstanbul", "Antalya", "İzmir", "Ankara" };
+            comboBoxPlace.DataSource = places;
+
+            // Add event handler to the name combobox to update the other comboboxes when the name changes
+            comboBoxNames.SelectedIndexChanged += ComboBoxNames_SelectedIndexChanged;
+        }
+
+        // When the selected name changes, randomly shuffle the values in the other comboboxes
+        private void ComboBoxNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            // Randomize hair color (since all are black in this case, we keep it black)
+            comboBoxHairColor.SelectedIndex = 0;  // Always black
+
+            // Randomize eye color (from the list of colors)
+            List<string> eyeColors = new List<string> { "Green", "Brown", "Black", "Blue", "Purple" };
+            comboBoxEyeColor.SelectedIndex = rand.Next(eyeColors.Count);
+
+            // Randomize place of living (from the list of places)
+            List<string> places = new List<string> { "Çanakkale", "İstanbul", "Antalya", "İzmir", "Ankara" };
+            comboBoxPlace.SelectedIndex = rand.Next(places.Count);
+        }
+
+
+
+
+        private void eyeColors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
